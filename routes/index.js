f6706307a014d61d112aaddae8b63807969ae73a
sebas413pa <sebasic_7@hotmail.com>
2025-09-11 21:38:56 -0600

    Actualización de routes/index.js
    Actualización de routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,10 @@ const router = express.Router();
 const saboresRoutes = require('./saboresRoutes');
 const ventasRoutes = require('./ventasRoutes');
 const productosRoutes = require('./productosRoutes');
-const clientesRoutes = require('./clientesRoutes');
 
 router.use('/sabores', saboresRoutes);
 router.use('/ventas', ventasRoutes);
 router.use('/productos', productosRoutes);
-router.use('/clientes', clientesRoutes);
 
 
 router.get('/', (req, res) => {
